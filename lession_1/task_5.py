318d2e29586b025ba54facf1a1afdaa2f218f262
Reanimator <ageev.mikhail.23@gmail.com>
2020-07-14 22:14:26 +0200

    Done task_5

--- lession_1/task_5.py
+++ lession_1/task_5.py
@@ -5,3 +5,15 @@
 Подсказки:
 --- используйте модуль chardet
 """
+
+import subprocess
+import chardet
+
+
+SITES = ["yandex.ru", "youtube.com"]
+for sites in SITES:
+    PING = subprocess.Popen(['ping', sites], stdout=subprocess.PIPE)
+    for line in PING.stdout:
+        result = chardet.detect(line)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
