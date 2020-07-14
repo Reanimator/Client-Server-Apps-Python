e3deec6fc369eddcb0f6d45b2242f7103f3e5732
Reanimator <ageev.mikhail.23@gmail.com>
2020-07-14 22:00:25 +0200

    Done task_4

--- lession_1/task_4.py
+++ lession_1/task_4.py
@@ -6,3 +6,19 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+
+
+def sandwich(string):
+    """all to bytes und back"""
+    byte_str = str.encode(string, encoding="utf-8")
+    print(byte_str)
+    print(type(byte_str))
+    str_str = bytes.decode(byte_str, encoding="utf-8")
+    print(str_str)
+    print(type(str_str))
+
+
+STR = ["разработка", "администрирование", "protocol", "standard"]
+
+for i in STR:
+    sandwich(i)
