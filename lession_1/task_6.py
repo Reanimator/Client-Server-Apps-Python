22b798ebc974988c4d29f37c5cda67165ab6c5dc
Reanimator <ageev.mikhail.23@gmail.com>
2020-07-15 12:18:37 +0200

    Done task_6

--- lession_1/task_6.py
+++ lession_1/task_6.py
@@ -11,3 +11,44 @@
 например, with open('test_file.txt', encoding='utf-8') as t_f
 невыполнение условия - минус балл
 """
+
+from chardet.universaldetector import UniversalDetector
+
+
+def to_bytes(strings):
+    """all to bytes"""
+    return bytes(strings, encoding="utf_8")
+
+
+def detect():
+    """encoding definition"""
+    detector = UniversalDetector()
+    with open('test_file.txt', 'rb') as file:
+        for line in file:
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result
+
+
+def write_str(strings):
+    """write to file"""
+    file = open('test_file.txt', 'wb')
+    for i in strings:
+        file.write(to_bytes(i + '\n'))
+    file.close()
+
+
+def open_utf():
+    """opening in UTF"""
+    with open('test_file.txt', encoding='utf-8') as file:
+        for string in file:
+            print(string, end='')
+
+
+STR = ["сетевое программирование", "сокет", "декоратор"]
+
+write_str(STR)
+print(detect())
+open_utf()
