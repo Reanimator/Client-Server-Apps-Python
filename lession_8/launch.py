f3cf1c0ed596e95f617c256eb4c84bed97a2720c
Reanimator <ageev.mikhail.23@gmail.com>
2020-08-19 17:20:29 +0200

    Done lession_8

--- lession_8/launch.py
+++ lession_8/launch.py
@@ -8,13 +8,6 @@ while True:
     elif ACT == 's':
         QUAN_CLIENT = int(input('Введите колическтво клиентов'))
         WINDOW.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        # WINDOW.append(subprocess.Popen('python client.py -n test1',
-        #                                creationflags=subprocess.CREATE_NEW_CONSOLE))
-        # WINDOW.append(subprocess.Popen('python client.py -n test2',
-        #                                creationflags=subprocess.CREATE_NEW_CONSOLE))
-        # WINDOW.append(subprocess.Popen('python client.py -n test3',
-        #                                creationflags=subprocess.CREATE_NEW_CONSOLE))
-
         for i in range(QUAN_CLIENT):
             WINDOW.append(subprocess.Popen(f'python client.py -n user{i+1}',
                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
